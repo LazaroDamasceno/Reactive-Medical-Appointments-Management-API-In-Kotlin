b38761d74e785f72c99850586ef14f2fa5d1f33c
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-18 00:34:09 -0300

    Changed CustomerRegistrationService

--- v1/src/main/kotlin/com/api/v1/customer/CustomerRegistrationServiceImpl.kt
+++ v1/src/main/kotlin/com/api/v1/customer/CustomerRegistrationServiceImpl.kt
@@ -2,6 +2,7 @@ package com.api.v1.customer
 
 import com.api.v1.user.DuplicatedSsnException
 import com.api.v1.user.User
+import com.api.v1.user.UserRepository
 import jakarta.validation.Valid
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.count
@@ -16,6 +17,9 @@ private class CustomerRegistrationServiceImpl: CustomerRegistrationService {
     @Autowired
     lateinit var customerRepository: CustomerRepository
 
+    @Autowired
+    lateinit var userRepository: UserRepository
+
     override suspend fun register(requestDto: @Valid CustomerRegistrationRequestDto): CustomerResponseDto {
         return withContext(Dispatchers.IO) {
             val checkIfGivenSsnIsDuplicated = customerRepository
@@ -25,7 +29,8 @@ private class CustomerRegistrationServiceImpl: CustomerRegistrationService {
             if (checkIfGivenSsnIsDuplicated) {
                 throw DuplicatedSsnException(requestDto.user.ssn)
             }
-            val customer = Customer(requestDto.user, requestDto.address)
+            val savedUser = userRepository.save(requestDto.user)
+            val customer = Customer(savedUser, requestDto.address)
             val savedCustomer = customerRepository.save(customer)
             CustomerResponseMapper.map(savedCustomer)
         }
