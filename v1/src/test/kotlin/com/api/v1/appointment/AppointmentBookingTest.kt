b0513a19d90b7ab5167b16bc202da387f7192698
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-25 01:22:33 -0300

    Created the test class AppointmentFishingTest

--- v1/src/test/kotlin/com/api/v1/appointment/AppointmentBookingTest.kt
+++ v1/src/test/kotlin/com/api/v1/appointment/AppointmentBookingTest.kt
@@ -20,18 +20,22 @@ private class AppointmentBookingTest {
     @Test
     @Order(1)
     fun testSuccessfulBooking() {
-        val requestDto = AppointmentSchedulingRequestDto(
-            "123456789",
-            "1234567",
-            LocalDateTime.parse("2024-12-12T09:30:00")
-        )
-        webTestClient
-            .post()
-            .uri("api/v1/appointments")
-            .bodyValue(requestDto)
-            .exchange()
-            .expectStatus()
-            .is2xxSuccessful()
+        var testcases = 30
+        while (testcases > 0) {
+            val requestDto = AppointmentSchedulingRequestDto(
+                "123456789",
+                "1234567",
+                LocalDateTime.parse("2024-12-12T09:30:00")
+            )
+            webTestClient
+                .post()
+                .uri("api/v1/appointments")
+                .bodyValue(requestDto)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+            testcases--
+        }
     }
 
     @Test
---
+++ v1/src/test/kotlin/com/api/v1/appointment/AppointmentFinishingTest.kt
@@ -0,0 +1,39 @@
+package com.api.v1.appointment
+
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+private class AppointmentFishingTest {
+
+    @Autowired
+    lateinit var webTestClient: WebTestClient
+
+    @Test
+    @Order(1)
+    fun testSuccessfulFinishing() {
+        webTestClient
+            .patch()
+            .uri("api/v1/appointments/20240001/finishing")
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
+    }
+
+    @Test
+    fun testUnsuccessfulFinishing() {
+        webTestClient
+            .patch()
+            .uri("api/v1/appointments/20240001/finishing")
+            .exchange()
+            .expectStatus()
+            .is5xxServerError()
+    }
+
+}
