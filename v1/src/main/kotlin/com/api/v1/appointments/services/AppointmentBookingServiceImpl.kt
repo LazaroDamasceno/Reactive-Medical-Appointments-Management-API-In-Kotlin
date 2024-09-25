502ad4409a93b6a1edbe7281205849d58d7311c1
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-09-25 03:25:20 -0300

    Changed the service AppointmentSchedulingService

--- v1/src/main/kotlin/com/api/v1/appointments/services/AppointmentBookingServiceImpl.kt
+++ v1/src/main/kotlin/com/api/v1/appointments/services/AppointmentBookingServiceImpl.kt
@@ -4,11 +4,15 @@ import com.api.v1.appointments.domain.Appointment
 import com.api.v1.appointments.domain.AppointmentRepository
 import com.api.v1.appointments.dtos.AppointmentResponseDto
 import com.api.v1.appointments.dtos.AppointmentSchedulingRequestDto
+import com.api.v1.appointments.exceptions.InvalidAppointmentException
 import com.api.v1.appointments.utils.AppointmentResponseMapper
+import com.api.v1.customers.domain.Customer
 import com.api.v1.customers.utils.CustomerFinderUtil
 import com.api.v1.doctors.utils.DoctorFinderUtil
 import jakarta.validation.Valid
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.singleOrNull
 import kotlinx.coroutines.withContext
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -30,9 +34,32 @@ private class AppointmentSchedulingServiceImpl: AppointmentBookingService {
         return withContext(Dispatchers.IO) {
             val customer = customerFinderUtil.find(responseDto.ssn)
             val doctor = doctorFinderUtil.find(responseDto.licenseNumber)
-            val appointment = Appointment(doctor, customer, LocalDateTime.parse("2025-01-12T09:30:00"))
+            isAppointmentInvalid(customer, responseDto.bookingDate)
+            val appointment = Appointment(doctor, customer, responseDto.bookingDate)
             val scheduledAppointment = appointmentRepository.save(appointment)
             AppointmentResponseMapper.map(scheduledAppointment)
+        }
+    }
+
+    suspend fun isAppointmentInvalid(customer: Customer, bookingDate: LocalDateTime) {
+        val day = LocalDateTime.now().dayOfMonth
+        val month = LocalDateTime.now().monthValue
+        val year = LocalDateTime.now().year
+        val ldt = LocalDateTime.of(year, month, day, 0, 0, 0)
+        if (bookingDate.isEqual(ldt) || bookingDate.isBefore(ldt)) {
+            val message = "The booking date must after today."
+            throw InvalidAppointmentException(message)
+        }
+        val isGivenBookingDateAlreadyBooked = appointmentRepository
+            .findAll()
+            .filter { e -> e.bookedDate == bookingDate
+                    && (e.finishedAt != null || e.canceledAt != null)
+                    && e.customer == customer
+            }
+            .singleOrNull() != null
+        if (isGivenBookingDateAlreadyBooked) {
+            val message = "The booking date is already booked."
+            throw InvalidAppointmentException(message)
         }
     }
 
--- v1/src/test/kotlin/com/api/v1/appointment/AppointmentBookingTest.kt
+++ v1/src/test/kotlin/com/api/v1/appointment/AppointmentBookingTest.kt
@@ -20,22 +20,18 @@ private class AppointmentBookingTest {
     @Test
     @Order(1)
     fun testSuccessfulBooking() {
-        var testcases = 30
-        while (testcases > 0) {
-            val requestDto = AppointmentSchedulingRequestDto(
-                "123456789",
-                "1234567",
-                LocalDateTime.parse("2024-12-12T09:30:00")
-            )
-            webTestClient
-                .post()
-                .uri("api/v1/appointments")
-                .bodyValue(requestDto)
-                .exchange()
-                .expectStatus()
-                .is2xxSuccessful()
-            testcases--
-        }
+        val requestDto = AppointmentSchedulingRequestDto(
+            "123456789",
+            "1234567",
+            LocalDateTime.parse("2024-12-12T09:30:00")
+        )
+        webTestClient
+            .post()
+            .uri("api/v1/appointments")
+            .bodyValue(requestDto)
+            .exchange()
+            .expectStatus()
+            .is2xxSuccessful()
     }
 
     @Test
